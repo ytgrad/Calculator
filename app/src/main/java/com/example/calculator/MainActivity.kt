675bea341ee6e16ef80f6045dc1b4e672fc15c92
ytgrad <ytgrad17@gmail.com>
2024-03-21 14:52:42 +0530

    added ui

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view:View){
+        Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
     }
 }
